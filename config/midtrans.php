ce38a9a29bd08e5fb5690c4476595cc9c5838111
Guido Famula <guidofamula29@gmail.com>
2023-07-01 00:39:37 +0700

    minor update

--- config/midtrans.php
+++ config/midtrans.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\ServiceProvider;
 
 return [
-    'merchant_id' => env('MIDTRANS_MERCHANT_ID'),
-    'client_key' => env('MIDTRANS_CLIENT_KEY'),
-    'server_key' => env('MIDTRANS_SERVER_KEY'),
+  'merchant_id' => env('MIDTRANS_MERCHANT_ID'),
+  'client_key' => env('MIDTRANS_CLIENT_KEY'),
+  'server_key' => env('MIDTRANS_SERVER_KEY'),
 ];
